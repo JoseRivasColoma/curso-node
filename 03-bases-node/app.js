92f971e1bd39bb1e0227dc0605e974d3f0e73d12
Jose Luis Rivas <jose.rivas@anidalatam.com>
2021-10-20 23:30:18 -0300

    Conociendo process.argv

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -4,7 +4,17 @@ const { crearArchivoTabla } = require('./helpers/multiplicar');
 //Limpia la consola
 console.clear();
 
-crearArchivoTabla(1564)
+//Recibir argumentos consola
+console.log(process.argv)
+//Tiene inconvenientes debido al escaso control sobre los parametros
+//Esto es demasiado volatil
+const [,,arg3='base=5'] = process.argv;
+const [, base = 5] = arg3.split('=')
+
+console.log(base);
+
+
+crearArchivoTabla(base)
     .then( salida => console.log(salida))
     .catch( err => console.log(err));
 
