88030d2bbbb7a1e52e4031ec06f291fc5db6963b
Jose Luis Rivas <jose.rivas@anidalatam.com>
2021-11-22 12:48:42 -0300

    Backend Basico Listo

---
+++ 07.2-rest-server/routes/usuarios.routes.js
@@ -0,0 +1,18 @@
+const { Router } = require('express');
+const { usuariosGet, usuariosPost, usuariosPatch, usuariosPut, usuariosDelete } = require('../controllers/usuarios.controller');
+
+const router = Router();
+
+router.get('/', usuariosGet);
+
+router.put('/:id', usuariosPut);
+
+router.post('/', usuariosPost);
+
+router.delete('/', usuariosDelete);
+
+router.patch('/', usuariosPatch);
+
+module.exports = router;
+
+
